773fafbfeef61f9c64f0d3edf09ffa52bf1e9eb6
Nuwan Walisundara <nuwan.walisundara@gmail.com>
2018-10-08 06:07:25 +0530

    configserver

---
+++ config-clent-app/src/main/java/com/nuwan/configclentapp/ConfigClentAppApplication.java
@@ -0,0 +1,14 @@
+package com.nuwan.configclentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigClentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClentAppApplication.class, args);
+	}
+}
