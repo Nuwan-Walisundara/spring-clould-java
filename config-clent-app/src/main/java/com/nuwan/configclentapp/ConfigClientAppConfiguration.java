3ee3f1beadb3b92b1c216f574e6c61834e6a8d0e
Nuwan Walisundara <nuwan.walisundara@gmail.com>
2018-10-11 05:00:25 +0530

    addd config for client

---
+++ config-clent-app/src/main/java/com/nuwan/configclentapp/ConfigClientAppConfiguration.java
@@ -0,0 +1,20 @@
+package com.nuwan.configclentapp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "some")
+public class ConfigClientAppConfiguration {
+	private String properties;
+
+	public String getProperties() {
+		return properties;
+	}
+
+	
+	public void setProperties(String properties) {
+		this.properties = properties;
+	}
+
+}
