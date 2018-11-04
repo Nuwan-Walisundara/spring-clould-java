75f35349f3c1e2a1e07f68a5bf55d07fbf363b3f
NuwanW <nwalisundara@virtusa.com>
2018-11-04 13:57:40 +0530

    zull init

---
+++ goodbye-service/src/main/java/com/nuwan/goodbyeservice/GoodbyeServiceApplication.java
@@ -0,0 +1,22 @@
+package com.nuwan.goodbyeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class GoodbyeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodbyeServiceApplication.class, args);
+	}
+	
+	@RequestMapping
+	public String goodBye() {
+		return "Good Bye";
+	}
+}
