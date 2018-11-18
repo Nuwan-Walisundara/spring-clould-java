8c6cdd5314657fec5c2e6bbc2b47bf11f2fcba5b
NuwanW <nwalisundara@virtusa.com>
2018-11-18 14:34:31 +0530

    adding customize conafic for ribon

--- ribon-time-app/src/main/java/com/nuwan/ribontimeapp/RibonTimeAppApplication.java
+++ ribon-time-app/src/main/java/com/nuwan/ribontimeapp/RibonTimeAppApplication.java
@@ -5,14 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.nuwan.ribontimeapp.config.RibbonTimeConfig;
+
 @RestController
 @SpringBootApplication
-@EnableDiscoveryClient
+@RibbonClient(name="time-service",configuration=RibbonTimeConfig.class)
 public class RibonTimeAppApplication {
 	@Autowired
 	private RestTemplate restTemplate;
